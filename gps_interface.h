e8a1f82b67c6a9a4cbd2a86f818c2a49714a99b9
Davide Andreolli <dvd2003333@gmail.com>
2023-03-05 14:36:04 +0100

    File loader added

--- gps_interface.h
+++ gps_interface.h
@@ -4,16 +4,21 @@
 
 #include "gps.h"
 
+enum SERIAL_MODE {
+    USB,
+    LOG_FILE
+};
+
 typedef struct gps_serial_port {
-  char *port;
-  int open;
-  int fd;
+    enum SERIAL_MODE type;
+    char* port;
+    int open;
+    int fd;
+    uint64_t last_timestamp;
 } gps_serial_port;
 
-int gps_interface_open(gps_serial_port *new_serial_port, const char *port,
-                       speed_t speed);
-void gps_interface_close(gps_serial_port *serial_port);
+int gps_interface_open(gps_serial_port* new_serial_port, const char* port, speed_t speed);
+int gps_interface_open_file(gps_serial_port* new_serial_port, const char *filename);
+void gps_interface_close(gps_serial_port* serial_port);
 
-gps_protocol_type gps_interface_get_line(
-    gps_serial_port *port, char start_sequence[GPS_MAX_START_SEQUENCE_SIZE],
-    int *start_sequence_size, char line[GPS_MAX_LINE_SIZE], int *line_size);
+gps_protocol_type gps_interface_get_line(gps_serial_port* port, char start_sequence[GPS_MAX_START_SEQUENCE_SIZE], int* start_sequence_size, char line[GPS_MAX_LINE_SIZE], int* line_size);
