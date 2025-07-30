c7f39840aebc1678e581e7222886998842f23174
nicolomarconi02 <nicolo.marconi02@gmail.com>
2025-07-30 14:34:31 +0200

    feat: udp port non blocking

--- gps_interface.c
+++ gps_interface.c
@@ -208,6 +208,9 @@ int gps_interface_open_udp(gps_serial_port *port, const char *udp_port) {
     exit(EXIT_FAILURE);
   }
 
+  int flags = fcntl(port->fd, F_GETFL, 0);
+  fcntl(port->fd, F_SETFL, flags | O_NONBLOCK);
+
   if (bind(port->fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
     printf("GPS Interface: Error binding\n");
     return -1;
